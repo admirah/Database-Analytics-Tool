5dcc37f6e14dcd15386af568455ca889d0517b8d
Vejsil Hrustic <vejsilh@outlook.com>
2016-11-13 13:59:21 +0100

    dodatne modifikacije na tabovima

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,8 +2,5 @@
 
 angular.module('datApp')
   .controller('MainCtrl', function ($scope, $http, $state) {
-    $(document).ready(function(){
-        $('ul.tabs').tabs();
-      });
     $state.go('general');
 });
