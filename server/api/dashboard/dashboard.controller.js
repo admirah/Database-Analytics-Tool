e2147881bbab0cc3f623192b9aba963456f1d56c
Admira Husic <admira.husicc@gmail.com>
2016-11-14 16:37:36 +0100

    fixed route

--- server/api/dashboard/dashboard.controller.js
+++ server/api/dashboard/dashboard.controller.js
@@ -24,8 +24,9 @@ console.log("hej")
 
 exports.getStudentGrades = function(req, res) {
   console.log("ansifjsiofjsdigjsiogjiojg");
-  sequelize.query("SElECT DISTINCT CONCAT(x.ime,' ',x.prezime) as 'ime', p.naziv , o.oc" +
-      "jena from predmet p, konacna_ocjena o,osoba x where x.id=student and student=" + req.params.id, {type: sequelize.QueryTypes.SELECT})
+  sequelize.query("SElECT DISTINCT CONCAT(x.ime,' ',x.prezime) as 'ime', p.naziv , " +
+    "  o.ocjena from predmet p, konacna_ocjena o,osoba x where x.id=o.student " +
+    "and p.id =o.predmet  and o.student" + req.params.id, {type: sequelize.QueryTypes.SELECT})
     .then(function (users) {
       return res.json(users);
     });
